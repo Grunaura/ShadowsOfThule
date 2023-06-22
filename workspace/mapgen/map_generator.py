ed43197a1020b126b0ddc16e94761851f5eaef07
Grunaura <37161764+Grunaura@users.noreply.github.com>
2023-06-21 17:58:27 -0700

    Update map_generator.py

--- workspace/mapgen/map_generator.py
+++ workspace/mapgen/map_generator.py
@@ -1,15 +1,98 @@
+# map_generator.py
+
+import random
+import os.path
+from location_description import *
+
 class Map:
     def __init__(self, size=(100, 100)):
         self.size = size
         self.contents = []
 
     def generate(self):
-        pass  # generate the contents here
+        if os.path.exists('location_description.py'):
+            self.generate_maps_from_description()
+        else:
+            self.generate_maps_organically()
 
-class Location:
-    def __init__(self, type):
-        self.type = type
-        self.contains = []
+    def generate_maps_from_description(self):
+        # Read location_description.py and generate maps based on the data
+        # ...
 
-def generate_locations():
-    return [Location('forest'), Location('desert')]  # as an example
+    def generate_maps_organically(self):
+        # Generate maps organically from the bottom up
+        maps = []
+        for continent in continents:
+            continent_map = self.generate_continent_map(continent)
+            maps.append(continent_map)
+
+        with open('maps.py', 'w') as f:
+            f.write("from map_classes import Map\n\n")
+            for i, map in enumerate(maps):
+                f.write(f"map_{i+1} = Map({map.size})\n")
+                f.write(f"map_{i+1}.contents = {map.contents}\n")
+
+    def generate_continent_map(self, continent):
+        # Generate the map for a continent
+        continent_map = Map()
+        # ...
+
+        for country in continent.countries:
+            country_map = self.generate_country_map(country)
+            continent_map.contents.append(country_map)
+
+        return continent_map
+
+    def generate_country_map(self, country):
+        # Generate the map for a country
+        country_map = Map()
+        # ...
+
+        for city in country.cities:
+            city_map = self.generate_city_map(city)
+            country_map.contents.append(city_map)
+
+        return country_map
+
+    def generate_city_map(self, city):
+        # Generate the map for a city
+        city_map = Map()
+        # ...
+
+        for building in city.buildings:
+            building_map = self.generate_building_map(building)
+            city_map.contents.append(building_map)
+
+        return city_map
+
+    def generate_building_map(self, building):
+        # Generate the map for a building
+        building_map = Map()
+        # ...
+
+        return building_map
+
+    def generate_castle_map(self, castle):
+        # Generate the map for a castle
+        castle_map = Map()
+        # ...
+
+        return castle_map
+
+    def generate_dungeon_map(self, dungeon):
+        # Generate the map for a dungeon
+        dungeon_map = Map()
+        # ...
+
+        return dungeon_map
+
+
+def generate_maps():
+    map = Map()
+    map.generate()
+    return map
+
+
+if __name__ == "__main__":
+    generated_map = generate_maps()
+    # Use the generated_map for the game
